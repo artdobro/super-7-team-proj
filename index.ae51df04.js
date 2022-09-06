f65bc19d22d865d807ac0b4f5717a7ce6efe4284
IrynaSokolova-Quanandrya <80633345+IrynaSokolova-Quanandrya@users.noreply.github.com>
2022-09-06 16:41:47 +0000

    Deploying to gh-pages from @ IrynaSokolova-Quanandrya/super-7-team-proj@12cf14468d8c747781df3490a0c7f49ed478846a 🚀

---
+++ index.ae51df04.js
@@ -0,0 +1,2 @@
+document.querySelector(".js-speaker-form").addEventListener("submit",(e=>{e.preventDefault(),new FormData(e.currentTarget).forEach(((e,r)=>console.log(`${r}: ${e}`))),e.currentTarget.reset()}));
+//# sourceMappingURL=index.ae51df04.js.map
